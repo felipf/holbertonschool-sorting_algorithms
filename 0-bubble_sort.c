4bcc8240339acb5d713d3acf0ccacd921cc0f62b
felipf <6341@holbertonstudents.com>
2023-05-19 10:27:55 -0700

    task 0 nowv2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,19 +6,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        int i, j, tmp;
+	size_t i, j;
+	int tmp;
 
-        for (i = 0; 1 < size - 1; i++)
-        {
-                for (j = 0; j < (int)size - i - 1; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                tmp = array[j];
-                                array[j] = array[j + 1];
-                                array [j + 1] = tmp;
-                                print_array(array, size);
-                        }
-                }
-        }
+	if (array == NULL)
+		return;
+
+	for (i = 0; 1 < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array [j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
