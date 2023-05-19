5c88d211e7ea98e6941950e0b963292fe6ade788
felipf <6341@holbertonstudents.com>
2023-05-19 10:06:02 -0700

    task0 and files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+/**
+ * bubble_sort - func that sorts an array of ints in ascending order
+ * @size: size of array
+ * @array: array to be sorted
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, tmp;
+
+	for (i = 0; 1 < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array [j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
